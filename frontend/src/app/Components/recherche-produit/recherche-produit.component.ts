e095ea8e36c734d5b23fd064272dc5db8ef469cc
SpeedsKillsx <amayaslabchri88@gmail.com>
2025-02-06 03:31:40 +0100

    prodSearch-created

---
+++ frontend/src/app/Components/recherche-produit/recherche-produit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recherche-produit',
+  standalone: true,
+  imports: [],
+  templateUrl: './recherche-produit.component.html',
+  styleUrl: './recherche-produit.component.css'
+})
+export class RechercheProduitComponent {
+
+}

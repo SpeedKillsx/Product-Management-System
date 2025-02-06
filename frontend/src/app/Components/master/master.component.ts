def4af3d7b9b8ec2b7358e165b2518f9933173d2
SpeedsKillsx <amayaslabchri88@gmail.com>
2025-02-06 01:06:50 +0100

    master-frontend-style

--- frontend/src/app/Components/master/master.component.ts
+++ frontend/src/app/Components/master/master.component.ts
@@ -3,11 +3,12 @@ import { ProduitListComponentComponent } from '../produit-list-component/produit
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ProduitFormComponentComponent } from '../produit-form-component/produit-form-component.component';
+import { ProduitDetailComponent } from '../produit-detail/produit-detail.component';
 
 @Component({
   selector: 'app-master',
   standalone: true,
-  imports: [FormsModule, CommonModule, ProduitFormComponentComponent],
+  imports: [FormsModule, CommonModule, ProduitFormComponentComponent, ProduitDetailComponent],
   templateUrl: './master.component.html',
   styleUrl: './master.component.css'
 })

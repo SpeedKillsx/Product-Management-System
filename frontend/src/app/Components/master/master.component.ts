de63d10fa299a355ba2a3536c8e48f62a78115d9
SpeedsKillsx <amayaslabchri88@gmail.com>
2025-02-06 03:42:22 +0100

    route-prodSearch

--- frontend/src/app/Components/master/master.component.ts
+++ frontend/src/app/Components/master/master.component.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ProduitFormComponentComponent } from '../produit-form-component/produit-form-component.component';
 import { ProduitDetailComponent } from '../produit-detail/produit-detail.component';
+import { RechercheProduitComponent } from '../recherche-produit/recherche-produit.component';
 
 @Component({
   selector: 'app-master',
   standalone: true,
-  imports: [FormsModule, CommonModule, ProduitFormComponentComponent, ProduitDetailComponent],
+  imports: [FormsModule, CommonModule, ProduitFormComponentComponent, ProduitDetailComponent, RechercheProduitComponent],
   templateUrl: './master.component.html',
   styleUrl: './master.component.css'
 })

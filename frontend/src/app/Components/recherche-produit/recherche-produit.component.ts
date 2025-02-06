f782e92b5a4c0f034f058b7fa0221982710159c8
SpeedsKillsx <amayaslabchri88@gmail.com>
2025-02-06 16:17:47 +0100

    ProdSearch-done

--- frontend/src/app/Components/recherche-produit/recherche-produit.component.ts
+++ frontend/src/app/Components/recherche-produit/recherche-produit.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { Product } from '../../model/class/Product';
+import { ProduitSearchService } from '../../services/ProduitSearchService/produit-search.service';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Category } from '../../model/class/Category';
+import { ProduitFormService } from '../../services/ProduitFormService/produit-form-component.service';
+import { APIData } from '../../model/interface/APIData';
+import { error } from 'console';
 
 @Component({
   selector: 'app-recherche-produit',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './recherche-produit.component.html',
   styleUrl: './recherche-produit.component.css'
 })
-export class RechercheProduitComponent {
+export class RechercheProduitComponent implements OnInit{
+  // Lists
+  producList: Product[] = [];
+  categoriesList: Category[] = [];
+  filtredProductList: Product[] = [];
+  //Inject Services
+  prodSearchService = inject(ProduitSearchService);
+  prodListService = inject(ProduitFormService);
+  // Form
+  groupForm: FormGroup = new FormGroup({
+    name: new FormControl(""),
+    description: new FormControl(""),
+    price: new FormControl(0),
+    quantity: new FormControl(0),
+    category_name: new FormControl(0),
+  });
+
+
+  ngOnInit(): void {
+    this.prodListService.loadCategories().subscribe((res:any)=>{
+      this.categoriesList = res;
+    });
+  }
+  
+  getProductMulti() {
+    const formValue = this.groupForm.value;
+    console.log(formValue);
+    this.prodSearchService.searchProductMulti(formValue).subscribe((res:APIData)=>{
+      this.filtredProductList = res.data;
+
+    }, (error)=>{
+      alert("Error during the research with the sepecefique parameters");
+      console.log("ERROR: ", error);
+    });
+  }
+
 
 }
--- frontend/src/app/services/ProduitSearchService/produit-search.service.ts
+++ frontend/src/app/services/ProduitSearchService/produit-search.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { APIData } from '../../model/interface/APIData';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProduitSearchService {
 
-  constructor() { }
+  http = inject(HttpClient);
+  
+  searchProductMulti(filters?: {
+    name?: string;
+    description?: string;
+    price?: number;
+    quantity?: number;
+    category_name?: string
+
+  }): Observable<APIData> {
+    let params = new HttpParams();
+    
+    if (filters?.name) params = params.set('name', filters.name);
+    if (filters?.description) params = params.set('description', filters.description);
+    if (filters?.price) params = params.set('price', filters.price.toString());
+    if (filters?.quantity) params = params.set('quantity', filters.quantity.toString());
+    if (filters?.category_name) params = params.set('category_name', filters.category_name);
+    console.log("filters = ", params);
+
+    return this.http.get<APIData>(environment.API_URL + "produits/search", {params: params});
+  }
 }
